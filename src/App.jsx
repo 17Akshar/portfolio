116820dfeb18657c2eb03362699d7b18b0685f47
17Akshar <aksharparmar2002@gmail.com>
2023-06-15 14:33:03 +0530

    Routing added!

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
 import Home from './components/Home';
+import { BrowserRouter, Routes, Route, Router, Navigate } from "react-router-dom";
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Ps from './components/Ps'
 import Contact from './components/Contact'
+import NotFound from './components/NotFound';
+import Edu from './components/Edu';
+import Ach from './components/Ach';
+import Project from './components/Project';
+import Skills from './components/Skills';
 function App() {
   return (
-   
+    <BrowserRouter>
+
     <div className='main h-[100vh] w-full bg-black flex items-center text-gray-100 sm:flex-row flex-col-reverse'>
         <div className='navbar sm:h-full sm:w-40 w-full h-16 bg-black '>
           <Navbar/>
         </div>
         <div className='sub-main w-full h-[calc(100vh-4rem)] bg-black sm:h-full sm:w-[calc(100%-10rem)]'>
-          <Contact/>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/home' element={<Home/>}/>
+              <Route path='/about' element={<About/>}>
+                <Route path='education' element={<Edu/>}/>
+                <Route path='achivements' element={<Ach/>}/>
+              </Route>
+              <Route path='/project' element={<Ps/>}>
+                <Route path='projects' element={<Project/>}/>
+                <Route path='skills' element={<Skills/>}/>
+              </Route>
+              <Route path='/contact/' element={<Contact/>}/>
+              <Route path='/*' element={<NotFound/>}/>
+            </Routes>
         </div>
     </div>
-   
+    </BrowserRouter>
+
   );
 }
 
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
-
+import {useLocation} from 'react-router-dom'
 const Heading = () => {
+ 	const location = useLocation();
+  console.log(location.pathname)
+  if(location.pathname === '/project/projects' || location.pathname === '/project/skills'){
+    return (
+      <div>
+          <div className="header sm:text-4xl text-3xl font-semibold opacity-50 ">
+              <h1>Project and <span className='text-[#FA5A00]'>Skills</span></h1>
+          </div>
+      </div>
+    )
+  }
+else if(location.pathname === '/about/education' || location.pathname === '/about/achivements'){
+  return (
+    <div>
+        <div className="header sm:text-4xl text-3xl font-semibold opacity-50 ">
+            <h1>About <span className='text-[#FA5A00]'>Me</span></h1>
+        </div>
+    </div>
+  )
+}
+else if(location.pathname === '/contact'){
   return (
     <div>
         <div className="header sm:text-4xl text-3xl font-semibold opacity-50 ">
@@ -8,6 +29,16 @@ const Heading = () => {
         </div>
     </div>
   )
+}
+else{
+  return (
+    <div>
+        <div className="header sm:text-4xl text-3xl font-semibold opacity-50 ">
+            <h1>Ho <span className='text-[#FA5A00]'>me</span></h1>
+        </div>
+    </div>
+  )
+}
 }
 
 export default Heading
